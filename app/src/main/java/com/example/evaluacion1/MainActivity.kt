9e2dc42b8abd31096a675470f4e5a19fa17857e5
Carlos Paredes <00028516@uca.edu.sv>
2019-03-29 16:12:04 -0600

    Terminado con botonoes

--- app/src/main/java/com/example/evaluacion1/MainActivity.kt
+++ app/src/main/java/com/example/evaluacion1/MainActivity.kt
@@ -3,128 +3,63 @@ package com.example.evaluacion1
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
-    private var counter1 = 0
-    private var counter1Key = "insideCounter1"
-    private var counter2 = 0
-    private var counter2Key = "insideCounter1"
-    private var counter3 = 0
-    private var counter3Key = "insideCounter1"
-    private var counter4 = 0
-    private var counter4Key = "insideCounter1"
-    private var counter5 = 0
-    private var counter5Key = "insideCounter1"
-    private var counter6 = 0
-    private var counter6Key = "insideCounter1"
-    private var counter7 = 0
-    private var counter7Key = "insideCounter1"
-    private var counter8 = 0
-    private var counter8Key = "insideCounter1"
-    private var counter9 = 0
-    private var counter9Key = "insideCounter1"
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+    override fun onClick(v: View?) {
+        if(v!!.equals(btn_enviar)){
+            enviardatos()
+        }
+        else{
+            var view = v as LinearLayout
+            var counter = view.getChildAt(1) as TextView
+            if(counter.text.toString().equals("#"))
+                counter.text = "1"
+            else {
+                var newNumber = counter.text.toString().toInt()+1
+                counter.text = newNumber.toString()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState != null) {
-            restoreCounters(savedInstanceState)
-        }
+        btn_product1.setOnClickListener(this)
+        btn_product2.setOnClickListener(this)
+        btn_product3.setOnClickListener(this)
+        btn_product4.setOnClickListener(this)
+        btn_product5.setOnClickListener(this)
+        btn_product6.setOnClickListener(this)
+        btn_product7.setOnClickListener(this)
+        btn_product8.setOnClickListener(this)
+        btn_product9.setOnClickListener(this)
 
-        addEventListener()
-
-        var intent : Intent = Intent(this, SecondActivity::class.java)
-
-
+        btn_enviar.setOnClickListener(this)
     }
 
-    private fun restoreCounters(savedInstanceState: Bundle) {
+    private fun enviardatos() {
+        var intent:Intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("name", et_user.text.toString())
+        intent.putExtra("email", et_email.text.toString())
+        intent.putExtra("product1", if((btn_product1.getChildAt(1)as TextView).text.toString() == "#") "0" else (btn_product1.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product2", (btn_product2.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product3", (btn_product3.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product4", (btn_product4.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product5", (btn_product5.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product6", (btn_product6.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product7", (btn_product7.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product8", (btn_product8.getChildAt(1)as TextView).text.toString())
+        intent.putExtra("product9", (btn_product9.getChildAt(1)as TextView).text.toString())
 
-        counter1 = savedInstanceState.getInt(counter1Key)
-        counter2 = savedInstanceState.getInt(counter2Key)
-        counter3 = savedInstanceState.getInt(counter3Key)
-        counter4 = savedInstanceState.getInt(counter4Key)
-        counter5 = savedInstanceState.getInt(counter5Key)
-        counter6 = savedInstanceState.getInt(counter6Key)
-        counter7 = savedInstanceState.getInt(counter7Key)
-        counter8 = savedInstanceState.getInt(counter8Key)
-        counter9 = savedInstanceState.getInt(counter9Key)
-
-        btn_result1.text = counter1.toString()
-        btn_result2.text = counter2.toString()
-        btn_result3.text = counter3.toString()
-        btn_result4.text = counter4.toString()
-        btn_result5.text = counter5.toString()
-        btn_result6.text = counter6.toString()
-        btn_result7.text = counter7.toString()
-        btn_result8.text = counter8.toString()
-        btn_result9.text = counter9.toString()
-
+        startActivity(intent)
     }
 
-    private fun addEventListener() {
 
-        btn_product1.setOnClickListener {
-            counter1++
-            btn_result1.text = counter1.toString()
-        }
 
-        btn_product2.setOnClickListener {
-            counter2++
-            btn_result2.text = counter2.toString()
-        }
 
-        btn_product3.setOnClickListener {
-            counter3++
-            btn_result3.text = counter3.toString()
-        }
-
-        btn_product4.setOnClickListener {
-            counter4++
-            btn_result4.text = counter4.toString()
-        }
-
-        btn_product5.setOnClickListener {
-            counter5++
-            btn_result5.text = counter5.toString()
-        }
-
-        btn_product6.setOnClickListener {
-            counter6++
-            btn_result6.text = counter6.toString()
-        }
-
-        btn_product7.setOnClickListener {
-            counter7++
-            btn_result7.text = counter7.toString()
-        }
-
-        btn_product8.setOnClickListener {
-            counter8++
-            btn_result8.text = counter8.toString()
-        }
-
-        btn_product9.setOnClickListener {
-            counter9++
-            btn_result9.text = counter9.toString()
-        }
-
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-        outState.putInt(counter1Key, counter1)
-        outState.putInt(counter2Key, counter2)
-        outState.putInt(counter3Key, counter3)
-        outState.putInt(counter4Key, counter4)
-        outState.putInt(counter5Key, counter5)
-        outState.putInt(counter6Key, counter6)
-        outState.putInt(counter7Key, counter7)
-        outState.putInt(counter8Key, counter8)
-        outState.putInt(counter9Key, counter9)
-    }
 }
--- app/src/main/java/com/example/evaluacion1/SecondActivity.kt
+++ app/src/main/java/com/example/evaluacion1/SecondActivity.kt
@@ -1,12 +1,50 @@
 package com.example.evaluacion1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_second.*
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), View.OnClickListener{
+    override fun onClick(v: View?) {
+        val shareintent:Intent = Intent()
+        shareintent.action= Intent.ACTION_SEND
+        shareintent.type = "text/plain"
+        shareintent.putExtra(Intent.EXTRA_TEXT, """
+            username: ${tvs_user.text}
+            email: ${tvs_email.text}
+            total: ${tvs_totalproductos.text}
+        """.trimIndent())
+        startActivity(shareintent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val intent:Intent = getIntent()
+        mostrar(intent)
+        btn_share.setOnClickListener(this)
+    }
+
+    private fun mostrar(intent: Intent) {
+        tvs_user.text = intent.getStringExtra("name")
+        tvs_email.text = intent.getStringExtra("email")
+        var total:Int = 0
+        for (i in 1..9){
+            var cont = intent.getStringExtra("product$i").toInt()
+            total = total + cont
+        }
+        tvs_totalproductos.text = total.toString()
+        (btn_product1.getChildAt(1) as TextView).text = intent.getStringExtra("product1")
+        (btn_product2.getChildAt(1) as TextView).text = intent.getStringExtra("product2")
+        (btn_product3.getChildAt(1) as TextView).text = intent.getStringExtra("product3")
+        (btn_product4.getChildAt(1) as TextView).text = intent.getStringExtra("product4")
+        (btn_product5.getChildAt(1) as TextView).text = intent.getStringExtra("product5")
+        (btn_product6.getChildAt(1) as TextView).text = intent.getStringExtra("product6")
+        (btn_product7.getChildAt(1) as TextView).text = intent.getStringExtra("product7")
+        (btn_product8.getChildAt(1) as TextView).text = intent.getStringExtra("product8")
+        (btn_product9.getChildAt(1) as TextView).text = intent.getStringExtra("product9")
     }
 }
